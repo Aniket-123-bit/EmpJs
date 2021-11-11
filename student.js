e62fb7b821c8c511ac7e6d3f9e9249ea206913cd
Aniket-123-bit <anisunny782@gmail.com>
2021-11-11 11:35:29 +0530

    Aniket[ADDED] code for js

---
+++ student.js
@@ -0,0 +1,18 @@
+class Student{
+   
+    constructor(){
+        this.id=id;
+        this.name=this.name;
+
+    }
+    display(){
+        console.log("id :",this.id);
+        console.log("name:",this.name);
+    }
+}
+function doSomethings(id,name){
+
+}
+
+
+// Student obj = new Student(10,"abc");
