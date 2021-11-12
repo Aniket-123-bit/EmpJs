933a499ab52f9bbd1a5802dca4f173bf71a6dc12
Aniket-123-bit <anisunny782@gmail.com>
2021-11-12 21:34:25 +0530

    [Aniket]ADDED code for Revision

---
+++ mymodule.js
@@ -0,0 +1,14 @@
+function cube (x){
+    return x * x * x;
+}
+ const foo = Math.PI + Math.SQRT2;
+
+ var graph = {
+    Option: {
+        color:'white',
+        thickness:'2px'
+
+
+     },
+     draw:
+ }
